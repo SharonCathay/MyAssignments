dc92a11681660851ed9d1a9a0620baee32933e57
SharonCathay <86116694+SharonCathay@users.noreply.github.com>
2024-04-02 17:05:12 +0530

    Add files via upload

---
+++ day1/FireFox.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class FireFox {
+
+	public static void main(String[] args) {
+		float browserVersion = 100.2f;
+		String browserName= "Firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		System.out.println(browserVersion + "\n" + browserName + "\n" + isVisible + "\n" + releaseYear + "\n"+ browserLogo );
+
+	}
+
+}
